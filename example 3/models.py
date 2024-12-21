6c5fbd72b6d154fa0dbb38d28c2a515ef55ad1ff
Ramzeer95 <150178680+Ramzeer95@users.noreply.github.com>
2024-12-21 17:55:13 +0100

    models.py
    models.py for example 3

---
+++ example 3/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Message(models.Model):
+    sender = models.CharField(max_length=50)
+    recipient = models.CharField(max_length=50)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
